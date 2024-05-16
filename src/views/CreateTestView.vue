e6829fed30db00ac438d32430055889edaa38b66
Mikolaj <mikolajwojcik011@gmail.com>
2024-05-16 17:10:34 +0200

    jetbrains test

--- src/views/CreateTestView.vue
+++ src/views/CreateTestView.vue
@@ -42,6 +42,9 @@ export default {
 			} as TestSchema
     },
     methods: {
+      justTest(){
+        console.log('jtest')
+      },
        handleUpdateIndexStyle({qInx ,index_style}: any){
         this.QuestionArr[qInx].index_style = index_style
       },
